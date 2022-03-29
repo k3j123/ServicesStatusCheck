6059f24c19668091eddbcd775eb275217be71bad
Trong <trong@atomicrobot.com>
2022-03-29 15:27:29 -0400

    Initial Commit

---
+++ Service Status/Service_StatusApp.swift
@@ -0,0 +1,17 @@
+//
+//  Service_StatusApp.swift
+//  Service Status
+//
+//  Created by Trong on 3/29/22.
+//
+
+import SwiftUI
+
+@main
+struct Service_StatusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
